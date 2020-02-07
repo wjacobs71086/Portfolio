875cdf14f927d5865a55d67c0799ec94fa169349
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-02-06 19:25:18 -0800

    updated header

--- src/NavLinks/navLinks.js
+++ src/NavLinks/navLinks.js
@@ -3,18 +3,52 @@ import headIcon from "../headIcon.png";
 import "./navLinks.css";
 
 export class NavLinks extends Component {
+  state = {
+    screenY: window.scrollY
+  };
+
+  componentDidMount() {
+    window.onscroll = () => {
+      const newScrollHeight = Math.ceil(window.scrollY / 50) * 50;
+      if (this.state.screenY !== newScrollHeight) {
+        this.setState({ screenY: newScrollHeight });
+      }
+    };
+  }
+
+  renderScrollHeader = () => {
+    if (this.state.screenY > 0) {
+      return (
+        <div style={{ position: "fixed" }} className="navLinks" id="navLinks">
+          <img src={headIcon} className="icon" alt="icon" />
+          <div className="navLinkContainer">
+            <a href="#myWork" className="navLink">
+              My work
+            </a>
+            <a href="#contact" className="navLink">
+              Contact
+            </a>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="navLinks" id="navLinks">
+          <img src={headIcon} className="icon" alt="icon" />
+          <div className="navLinkContainer">
+            <a href="#myWork" className="navLink">
+              My work
+            </a>
+            <a href="#contact" className="navLink">
+              Contact
+            </a>
+          </div>
+        </div>
+      );
+    }
+  };
   render() {
-    return (
-      <div className="navLinks" id="navLinks">
-        <img src={headIcon} className="icon" alt="icon" />
-        <a href="#myWork" className="navLink">
-          My work
-        </a>
-        <a href="#contact" className="navLink">
-          Contact
-        </a>
-      </div>
-    );
+    return <div>{this.renderScrollHeader()}</div>;
   }
 }
 
