baabc50d27e6d55460a9875b4f8cad7ba446cecc
wjacobs71086 <w.jacobs.632@gmail.com>
2021-10-09 16:05:21 -0700

    added new icons

--- src/AboutMe/aboutMe.js
+++ src/AboutMe/aboutMe.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
-import icon from "../headIcon_purple.png";
+import icon from "../aboutIcons/headIcon_purple.png";
+import iconGif from "/Users/wesleyjacobs/Documents/Coding/Projects/Thinkful/portfolioproject/src/649FDB85-5B43-4BE7-8601-23BA688079F9.gif"
+import partyIcon from '../aboutIcons/partyIcon.png'
+import cussIcon from '../aboutIcons/cussIcon.png'
+import thinkingIcon from '../aboutIcons/thinkingIcon.png'
+import lightbulbIcon from '../aboutIcons/lightbulbIcon.png'
+import callMeIcon from '../aboutIcons/callMeIcon.png'
+import cloudsIcon from '../aboutIcons/cloudsIcon.png'
+import computerIcon from '../aboutIcons/computerIcon.png'
+
+let icons = [icon, partyIcon, cussIcon, thinkingIcon, lightbulbIcon, callMeIcon, cloudsIcon, computerIcon]
 
 export class AboutMe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentIcon: icon
+    };
+  }
+
+handleClick = () => {
+  let randomIconIndex = Math.floor(Math.random() * icons.length)
+  this.setState({currentIcon:icons[randomIconIndex]})
+}
+
+
   render() {
     return (
       <section className="about-me-section" id="aboutMe">
-        <img src={icon} alt="icon" className="AboutMeIcon"></img>
+        <img src={this.state.currentIcon} alt="icon" className="AboutMeIcon" onClick={this.handleClick}></img>
         <h4 className="section-title">About Me</h4>
         <p className="about-me-body">
           I am a passionate and collaborative software developer in the LA area.
